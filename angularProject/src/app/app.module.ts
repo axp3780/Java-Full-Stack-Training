0ebe4b89f71b9be65d602a06bb1a0a4d3efc93f8
Arjun <parajuliar@gmail.com>
2023-06-06 09:13:22 -0500

    new angular project added with drectives included

---
+++ angularProject/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MyComponentComponent } from './my-component/my-component.component';
+import { SharedModule } from './shared/shared.module';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
